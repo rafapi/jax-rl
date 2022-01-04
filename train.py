5d4cc65aa64c7fd129dfc6741e2f32dee4b497d6
Rafa <rafa@elementai.com>
2022-01-04 18:52:54 +0000

    Use hydra for config

--- train.py
+++ train.py
@@ -2,47 +2,37 @@ import jax
 import haiku as hk
 
 import gym
+import hydra
+
+from omegaconf import DictConfig
 
 from src.memory import ReplayBuffer
 from src.agent import DQN
 from src.utils import epsilon_by_frame, plot
-# from IPython.display import clear_output
 
 
-NUM_EPISODES = 1000
-BATCH_SIZE = 32
-GAMMA = 0.99
-LEARNING_RATE = 1e-3
-SEED = 1729
-HIDDEN_UNITS = 50
-REPLAY_CAPACITY = 2000
-
-epsilon_start = 1.0
-epsilon_final = 0.01
-epsilon_decay = 500
-
-
-def main():
+@hydra.main(config_path="config", config_name="conf")
+def main(config: DictConfig):
     env_id = "CartPole-v0"
     env = gym.make(env_id)
 
-    replay_buffer = ReplayBuffer(REPLAY_CAPACITY, GAMMA)
+    replay_buffer = ReplayBuffer(config.replay_capacity, config.gamma)
 
     losses = []
     all_rewards = []
     episode_reward = 0
 
     num_actions = env.action_space.n
-    agent = DQN(num_actions=num_actions, hidden_units=HIDDEN_UNITS, learning_rate=LEARNING_RATE)
+    agent = DQN(num_actions=num_actions, hidden_units=config.hidden_units, learning_rate=config.learning_rate)
     sample_input = env.reset()
-    rng = hk.PRNGSequence(jax.random.PRNGKey(SEED))
+    rng = hk.PRNGSequence(jax.random.PRNGKey(config.seed))
     net_params, opt_state = agent.param_init(sample_input, rng)
     target_params = net_params
 
     state = env.reset()
-    print(f"Training agent for {NUM_EPISODES} episodes...")
-    for idx in range(1, NUM_EPISODES+1):
-        epsilon = epsilon_by_frame(idx, epsilon_start, epsilon_decay, epsilon_final)
+    print(f"Training agent for {config.num_episodes} episodes...")
+    for idx in range(1, config.num_episodes+1):
+        epsilon = epsilon_by_frame(idx, config.epsilon_start, config.epsilon_decay, config.epsilon_final)
 
         action = agent.select_action(net_params, next(rng), state, epsilon)
         next_state, reward, done, _ = env.step(int(action))
@@ -56,8 +46,8 @@ def main():
             all_rewards.append(episode_reward)
             episode_reward = 0
 
-        if len(replay_buffer) > BATCH_SIZE:
-            batch = replay_buffer.sample(BATCH_SIZE)
+        if len(replay_buffer) > config.batch_size:
+            batch = replay_buffer.sample(config.batch_size)
             net_params, opt_state, loss = agent.update_parameters(net_params, target_params,
                                                                   opt_state, batch)
             losses.append(float(loss))
